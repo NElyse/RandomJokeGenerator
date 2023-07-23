f9b7aa62eb5f7d12d902b533f886f4a3eeff13d2
NElyse <105349034+NElyse@users.noreply.github.com>
2023-07-23 19:59:02 +0300

    joke Generator

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JokeController;
 
 Route::get('/random-joke', [JokeController::class, 'getRandomJoke']);
-Route::get('/joke-categories', [JokeController::class, 'getJokeCategories']);
